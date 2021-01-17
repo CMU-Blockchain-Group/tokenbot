abf856d784f833c2a2229ec6bab636757a0d45c9
Sebastien La Duca <sladuca777@gmail.com>
2021-01-16 23:33:25 -0500

    nope outta that weird syntax

--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -1,11 +1,14 @@
 import { BotClient, BotEvent } from '../types'
 
 export default class Ready implements BotEvent {
-  constructor (private readonly client: BotClient) { }
+  private readonly client: BotClient
+  constructor(client: BotClient) {
+    this.client = client
+  }
 
-  name (): string { return 'Ready' }
+  name(): string { return 'Ready' }
 
-  public async run (): Promise<void> {
+  public async run(): Promise<void> {
     if (this.client.user !== null) {
       console.log(`${this.client.user.username} is running.`)
       await this.client.user.setPresence(this.client.settings.presence)

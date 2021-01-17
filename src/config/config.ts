8738494001c6f78f2bfb9aa22d986cbf55686bfc
Sebastien La Duca <sladuca777@gmail.com>
2021-01-17 11:18:37 -0500

    return to docker, render doesn't like discordjs

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import { BotSettings } from "../types";
 export const settings: BotSettings = {
   presence: {
     activity: {
-      name: "just keep hodling",
+      name: "bufficorn simulator",
       type: "PLAYING",
     },
   },
